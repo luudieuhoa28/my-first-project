8c1e1c18f80b463d054b1a5c5d2eec1d505020c4
luudieuhoa28 <47111643+luudieuhoa28@users.noreply.github.com>
2019-01-30 21:14:14 +0700

    1

---
+++ getAcc.c
@@ -0,0 +1,13 @@
+// Get username
+void getUsername(char username[])
+{
+	printf("Enter your username: ");
+	gets(username);
+}
+
+// Get password
+void getPassword(char password[])
+{
+	printf("Enter your password: ");
+	gets(password);
+}
---
+++ getAcc.h
@@ -0,0 +1,6 @@
+// Get username
+void getUsername(char username[]);
+
+// Get password
+void getPassword(char password[]);
+
